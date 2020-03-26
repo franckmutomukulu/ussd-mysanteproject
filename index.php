ac910ef3ce372f2b3eb9d9c83353756ca0f0c182
Franck Mutomukulu <46424357+franckmutomukulu@users.noreply.github.com>
2020-03-27 01:32:58 +0800

    sencond change
    mutli languange ussd part

--- index.php
+++ index.php
@@ -7,8 +7,13 @@ $serviceCode = $_POST["serviceCode"];
 //This is the first menu screen
 
 if ( $text == "" ) {
-  $response  = "CON Hi welcome, I can help you with Event Reservation \n";
-  $response .= "1. Enter 1 to continue";
+  $response  = "Hello, Bonjour, Mbote, Jambo,Ingeta.  \n";
+  $response .= "1. Fina 1  Lingala";
+  $response .= "2. Vinya 2 Swahili";
+  $response .= "3. Tshia 3 Tshiluba";
+  $response .= "4. Beta 4  Kikongo";
+  $response .= "5. Entre 5 Francais";
+  $response .= "6. Enter 6 English";
 }
 
 
@@ -16,9 +21,9 @@ if ( $text == "" ) {
 // Will be brought to this second menu screen
 
 else if ($text == "1") {
-  $response  = "CON  Pick a table for reservation below \n";
-  $response .= "1. Table for 2 \n";
-  $response .= "2. Table for 4 \n";
+  $response  = "Corona esila \n";
+  $response .= "1. Lopitalo 2 \n";
+  $response .= "2. yebisa 4 \n";
   $response .= "3. Table for 6 \n";
   $response .= "4. Table for 8 \n";
 }
@@ -113,7 +118,301 @@ else if ($text == "1*4*1*0") {
 }
 
 
+// Swahili
+
+else if ($text == "2") {
+  $response  = "Corona ihishe \n";
+  $response .= "1. hopitalo 2 \n";
+  $response .= "2. yebisa 4 \n";
+  $response .= "3. Table for 6 \n";
+  $response .= "4. Table for 8 \n";
+}
+
+
+//Menu for a user who selects '1' from the second menu above
+// Will be brought to this third menu screen
+else if ($text == "1*1") {
+  $response = "CON You are about to book a table for 2 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+else if ($text == "1*1*1") {
+  $response = "CON Table for 2 cost -N- 50,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+
+else if ($text == "1*1*1*1") {
+  $response = "END Your Table reservation for 2 has been booked";
+}
+
+
+else if ($text == "1*1*1*0") {
+  $response = "END Your Table reservation for 2 has been canceled";
+}
+
+// Menu for a user who selects "2" from the second menu above
+// Will be brought to this fourth menu screen
+else if ($text == "1*2") {
+  $response = "CON You are about to book a table for 4 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who selects "1" from the fourth menu screen
+else if ($text == "1*2*1") {
+  $response = "CON Table for 4 cost -N- 150,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+        
+else if ($text == "1*2*1*1") {
+  $response = "END Your Table reservation for 4 has been booked";
+}
+    
+else if ($text == "1*2*1*0") {
+  $response = "END Your Table reservation for 4 has been canceled";
+}
+
+// Menu for a user who enters "3" from the second menu above
+// Will be brought to this fifth menu screen
+else if ($text == "1*3") {
+  $response = "CON You are about to book a table for 6 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who enters "1" from the fifth menu
+else if ($text == "1*3*1") {
+  $response = "CON Table for 6 cost -N- 250,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+
+else if ($text == "1*3*1*1") {
+  $response = "END Your Table reservation for 6 has been booked";
+}
+        
+else if ($text == "1*3*1*0") {
+  $response = "END Your Table reservation for 6 has been canceled";
+}
+
+// Menu for a user who enters "4" from the second menu above
+// Will be brought to this sixth menu screen
+else if ($text == "1*4") {
+  $response = "CON You are about to book a table for 8 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who enters "1" from the sixth menu
+else if ($text == "1*4*1") {
+  $response = "CON Table for 8 cost -N- 250,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+                       
+else if ($text == "1*4*1*1") {
+  $response = "END Your Table reservation for 8 has been booked";
+}
+            
+else if ($text == "1*4*1*0") {
+  $response = "END Your Table reservation for 8 has been canceled";
+}
+
+//Tshiluba
+
+
+else if ($text == "3") {
+  $response  = "Corona esila \n";
+  $response .= "1. Lopitalo 2 \n";
+  $response .= "2. yebisa 4 \n";
+  $response .= "3. Table for 6 \n";
+  $response .= "4. Table for 8 \n";
+}
+
+
+//Menu for a user who selects '1' from the second menu above
+// Will be brought to this third menu screen
+else if ($text == "1*1") {
+  $response = "CON You are about to book a table for 2 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+else if ($text == "1*1*1") {
+  $response = "CON Table for 2 cost -N- 50,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+
+else if ($text == "1*1*1*1") {
+  $response = "END Your Table reservation for 2 has been booked";
+}
+
+
+else if ($text == "1*1*1*0") {
+  $response = "END Your Table reservation for 2 has been canceled";
+}
+
+// Menu for a user who selects "2" from the second menu above
+// Will be brought to this fourth menu screen
+else if ($text == "1*2") {
+  $response = "CON You are about to book a table for 4 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who selects "1" from the fourth menu screen
+else if ($text == "1*2*1") {
+  $response = "CON Table for 4 cost -N- 150,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+        
+else if ($text == "1*2*1*1") {
+  $response = "END Your Table reservation for 4 has been booked";
+}
+    
+else if ($text == "1*2*1*0") {
+  $response = "END Your Table reservation for 4 has been canceled";
+}
+
+// Menu for a user who enters "3" from the second menu above
+// Will be brought to this fifth menu screen
+else if ($text == "1*3") {
+  $response = "CON You are about to book a table for 6 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who enters "1" from the fifth menu
+else if ($text == "1*3*1") {
+  $response = "CON Table for 6 cost -N- 250,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+
+else if ($text == "1*3*1*1") {
+  $response = "END Your Table reservation for 6 has been booked";
+}
+        
+else if ($text == "1*3*1*0") {
+  $response = "END Your Table reservation for 6 has been canceled";
+}
+
+// Menu for a user who enters "4" from the second menu above
+// Will be brought to this sixth menu screen
+else if ($text == "1*4") {
+  $response = "CON You are about to book a table for 8 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who enters "1" from the sixth menu
+else if ($text == "1*4*1") {
+  $response = "CON Table for 8 cost -N- 250,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+                       
+else if ($text == "1*4*1*1") {
+  $response = "END Your Table reservation for 8 has been booked";
+}
+            
+else if ($text == "1*4*1*0") {
+  $response = "END Your Table reservation for 8 has been canceled";
+}
 //echo response
+else if ($text == "4") {
+  $response  = "Corona hh \n";
+  $response .= "1. Lopitalo 2 \n";
+  $response .= "2. yebisa 4 \n";
+  $response .= "3. Table for 6 \n";
+  $response .= "4. Table for 8 \n";
+}
+
+
+//Menu for a user who selects '1' from the second menu above
+// Will be brought to this third menu screen
+else if ($text == "1*1") {
+  $response = "CON You are about to book a table for 2 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+else if ($text == "1*1*1") {
+  $response = "CON Table for 2 cost -N- 50,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+
+else if ($text == "1*1*1*1") {
+  $response = "END Your Table reservation for 2 has been booked";
+}
+
+
+else if ($text == "1*1*1*0") {
+  $response = "END Your Table reservation for 2 has been canceled";
+}
+
+// Menu for a user who selects "2" from the second menu above
+// Will be brought to this fourth menu screen
+else if ($text == "1*2") {
+  $response = "CON You are about to book a table for 4 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who selects "1" from the fourth menu screen
+else if ($text == "1*2*1") {
+  $response = "CON Table for 4 cost -N- 150,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+        
+else if ($text == "1*2*1*1") {
+  $response = "END Your Table reservation for 4 has been booked";
+}
+    
+else if ($text == "1*2*1*0") {
+  $response = "END Your Table reservation for 4 has been canceled";
+}
+
+// Menu for a user who enters "3" from the second menu above
+// Will be brought to this fifth menu screen
+else if ($text == "1*3") {
+  $response = "CON You are about to book a table for 6 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who enters "1" from the fifth menu
+else if ($text == "1*3*1") {
+  $response = "CON Table for 6 cost -N- 250,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+
+else if ($text == "1*3*1*1") {
+  $response = "END Your Table reservation for 6 has been booked";
+}
+        
+else if ($text == "1*3*1*0") {
+  $response = "END Your Table reservation for 6 has been canceled";
+}
+
+// Menu for a user who enters "4" from the second menu above
+// Will be brought to this sixth menu screen
+else if ($text == "1*4") {
+  $response = "CON You are about to book a table for 8 \n";
+  $response .= "Please Enter 1 to confirm \n";
+}
+
+// Menu for a user who enters "1" from the sixth menu
+else if ($text == "1*4*1") {
+  $response = "CON Table for 8 cost -N- 250,000.00 \n";
+  $response .= "Enter 1 to continue \n";
+  $response .= "Enter 0 to cancel";
+}
+                       
+else if ($text == "1*4*1*1") {
+  $response = "END Your Table reservation for 8 has been booked";
+}
+            
+else if ($text == "1*4*1*0") {
+  $response = "END Your Table reservation for 8 has been canceled";
+}
 
 header('Content-type: text/plain');
 echo $response
